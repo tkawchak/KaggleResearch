0798117e3d1c8f838ecb5bed7cad41b5bb16bfb7
Tom Kawchak <tkawchak@gmail.com>
2016-10-07 01:01:12 -0400

    added feature extraction for names and cabin numbers

--- kaggle-titanic/kaggleCompetitionModel.py
+++ kaggle-titanic/kaggleCompetitionModel.py
@@ -9,26 +9,29 @@ import numpy as np
 import sklearn
 import sklearn.ensemble
 
+num_features = 10
+
 # read in the train dataset
 print('loading and processing training data...')
 df = pd.read_csv('train.csv', header=0)
 
 # get rid of these columns
-df = df.drop(['Cabin', 'PassengerId'], axis=1)
+df = df.drop(['PassengerId'], axis=1)
 
 # impute values that are missing
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 
-# drop entries with missing values
-#df = df.dropna(how='any')
-# SPLIT NAMES AND USE LEAVE ONE OUT METHOD
-# TO FORM FEATURES
-df['Name'] = [name.split()[0].replace(',', '') for name in df['Name'].values]
-#print(df['Name'].values)
-#from sklearn.model_selection import LeaveOneGroupOut
-#logo = LeaveOneGroupOut()
+# if a cabin value is know, assign 1, otherwise 0
+df['Cabin'] = df['Cabin'].replace(np.nan, '0')
+df['CabinVal'] = [1 if (x != '0') else 0 for x in df['Cabin']]
+df = df.drop(['Cabin'], axis=1)
 
+# leave only last names and count the number of entries with the same last name
+df['Name'] = [name.split()[0].replace(',', '') for name in df['Name'].values]
+df['NameCount'] = [df['Name'].values.tolist().count(x) for x in df['Name'].values.tolist()]
+# drop the name column because we no longer use it.
+# what other uses could we have???
 df = df.drop(['Name'], axis=1)
 
 # remove the leading letters from tickets.
@@ -42,13 +45,7 @@ sex_mapping = {label:idx for idx, label in enumerate(np.unique(df['Sex']))}
 df['Sex'] = df['Sex'].map(sex_mapping)
 
 # map the embarkation point to 0, 1, 2
-test = df['Embarked'].isnull().values
-#for i in test:
-	#print(test[i])
-
-#df['Embarked'] = df['Embarked'].replace(None, 'Q')
-#df['Embarked'][df.Embarked.isnull()] = df['Embarked'].dropna().mode().values
-df['Embarked'] = df['Embarked'].replace(np.nan, 'S')
+df['Embarked'] = df['Embarked'].replace(np.nan, 'C')
 embark_mapping = {label:idx for idx, label in enumerate(np.unique(df['Embarked']))}
 df['Embarked'] = df['Embarked'].map(embark_mapping)
 
@@ -100,28 +97,41 @@ train_data = imputer_training.transform(df.values)
 # into train and test data to evaluate performance
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.grid_search import RandomizedSearchCV
+from sklearn.grid_search import GridSearchCV
 
 #stratKFolds = StratifiedKFold(y=train_data[:, 0], n_folds=5, random_state=1, shuffle=True)
 #for k, (train_set, test_set) in enumerate(stratKFolds):
 print('tuning hyperparameters...')
-n_estimators_range = [1, 5, 10, 15, 20, 25, 30, 35, 40]
-max_features_range = [int(x) for x in range(1, 9)]
-max_depth_range = [2 ,4, 6, 8, 10]
+n_estimators_range = [5, 10, 15, 20, 25, 30, 35, 40]
+max_features_range = [int(x) for x in range(3, num_features+1)]
+max_depth_range = [5, 10, 15, 20, None]
 #print(n_estimators_range.values())
 tree = RandomForestClassifier(criterion='entropy',
 	random_state=1, n_jobs=-1)
 param_grid = {'n_estimators': n_estimators_range, 'max_features': max_features_range,
 	'max_depth': max_depth_range}
-rs = RandomizedSearchCV(estimator=tree, 
-	param_distributions=param_grid,
+#rs = RandomizedSearchCV(estimator=tree, 
+#	param_distributions=param_grid,
+#	scoring='accuracy',
+#	cv=3, n_jobs=-1)
+#rs = rs.fit(train_data[:, 1:num_features+1], train_data[:, 0])
+#print(rs.best_score_)
+#print(rs.best_params_)
+
+gs = GridSearchCV(estimator=tree,
+	param_grid=param_grid,
 	scoring='accuracy',
-	cv=5, n_jobs=-1)
-rs = rs.fit(train_data[:, 1:9], train_data[:, 0])
-print(rs.best_score_)
-print(rs.best_params_)
+	cv=3, n_jobs=-1)
+gs = gs.fit(train_data[:, 1:num_features+1], train_data[:, 0])
+print('best score from grid search: %.3f' % gs.best_score_)
+print(gs.best_params_)
+best = gs.best_params_
+n_estimators = best['n_estimators']
+max_depth = best['max_depth']
+max_features = best['max_features']
 
 #from sklearn.cross_validation import train_test_split
-#X_train, X_test, y_train, y_test = train_test_split(train_data[:, 1:9], train_data[:, 0],
+#X_train, X_test, y_train, y_test = train_test_split(train_data[:, 1:num_features+1], train_data[:, 0],
 #	test_size = 0.25, random_state=1)
 #tree1.fit(X_train, y_train)
 #tree2.fit(X_train, y_train)
@@ -154,25 +164,32 @@ from sklearn.metrics import accuracy_score
 
 print('training on all training data...')
 tree_test = RandomForestClassifier(criterion='entropy',
-	n_estimators=25, max_features=7, max_depth=10, random_state=1, n_jobs=-1)
-tree_test.fit(train_data[:, 1:9], train_data[:, 0])
+	n_estimators=n_estimators, max_features=max_features, max_depth=max_depth, random_state=1, n_jobs=-1)
+tree_test.fit(train_data[:, 1:num_features+1], train_data[:, 0])
 
 # read in the test data and predict the outputs
 print('reading and processing the input data...')
 df_test = pd.read_csv('test.csv', header=0)
 ids = df_test['PassengerId'].values
-df_test = df_test.drop(['Name', 'Cabin', 'PassengerId'], axis=1)
+df_test = df_test.drop(['PassengerId'], axis=1)
 df_test['Sex'] = df_test['Sex'].map(sex_mapping)
 df_test['Ticket'] = df_test['Ticket'].replace(['LINE'], ['0'])
 df_test['Ticket'] = [ticket.split()[len(ticket.split())-1] for ticket in df_test['Ticket'].values]
 df_test['Ticket'] = df_test['Ticket'].astype(int)
 df_test['Embarked'] = df_test['Embarked'].replace(np.nan, 'S')
 df_test['Embarked'] = df_test['Embarked'].map(embark_mapping)
+df_test['Name'] = [name.split()[0].replace(',', '') for name in df_test['Name'].values]
+df_test['NameCount'] = [df_test['Name'].values.tolist().count(x) for x in df_test['Name'].values.tolist()]
+df_test['Cabin'] = df_test['Cabin'].replace(np.nan, '0')
+df_test['CabinVal'] = [1 if (x != '0') else 0 for x in df_test['Cabin']]
+df_test = df_test.drop(['Cabin'], axis=1)
+df_test = df_test.drop(['Name'], axis=1)
 imputer2 = imputer.fit(df_test)
 test_data = imputer2.transform(df_test.values)
 print('predicting the output of the test data...')
-#test_pred = tree.predict(test_data).astype(int)
 test_pred = tree_test.predict(test_data).astype(int)
+#test_pred = tree.predict(test_data).astype(int)
+#test_pred = gs.predict(test_data).astype(int)
 
 # write output to a file
 import csv
