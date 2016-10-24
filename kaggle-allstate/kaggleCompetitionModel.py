9b4e05f29e28fb16275b7d2e673af46d022c9b5f
Tom Kawchak <tkawchak@gmail.com>
2016-10-23 22:56:30 -0400

    simple model for allstate Claims

--- kaggle-allstate/kaggleCompetitionModel.py
+++ kaggle-allstate/kaggleCompetitionModel.py
@@ -9,11 +9,17 @@ import numpy as np
 import sklearn
 import sklearn.ensemble
 from sklearn.preprocessing import Imputer
+import sys
 
 
 def build_features(df):
+	''' build the features to use for the model'''
+	
+	# remove the labels and the ids for use
+	y = df.pop('loss').values
+	ids = df.pop('id').values
+
 	# get rid of the columns that don't give us useful information
-	# ex: column ids 
 
 
 	# create an imputer for imputer values later
@@ -33,133 +39,47 @@ def build_features(df):
 			# do nothing here
 			a = 1
 
-	# categorical data: assign numerical values for each class
-	
-
-	# continuous variables: no processing should be required
-	# just make sure that these are numerical
-
-#	# if a cabin value is know, assign 1, otherwise 0
-#	df['Cabin'] = df['Cabin'].replace(np.nan, '0')
-#	df['CabinVal'] = [1 if (x != '0') else 0 for x in df['Cabin']]
-#	df = df.drop(['Cabin'], axis=1)
-#
-#	# leave only last names and count the number of entries with the same last name
-#	df['Name'] = [name.split()[0].replace(',', '') for name in df['Name'].values]
-#	df['NameCount'] = [df['Name'].values.tolist().count(x)-1 for x in df['Name'].values.tolist()]
-#	# drop the name column because we no longer use it.
-#	df = df.drop(['Name'], axis=1)
-#
-#	# remove the leading letters from tickets.
-#	# call split and then take the last element in the returned list
-#	df['Ticket'] = df['Ticket'].replace(['LINE'], ['0'])
-#	df['Ticket'] = [ticket.split()[len(ticket.split())-1] for ticket in df['Ticket'].values]
-#	df['Ticket'] = df['Ticket'].astype(int)
-#
-#	# map the sex label to 0, 1
-#	sex_mapping = {label:idx for idx, label in enumerate(np.unique(df['Sex']))}
-#	df['Sex'] = df['Sex'].map(sex_mapping)
-#
-#	# map the embarkation point to 0, 1, 2
-#	df['Embarked'] = df['Embarked'].replace(np.nan, 'C')
-#	embark_mapping = {label:idx for idx, label in enumerate(np.unique(df['Embarked']))}
-#	df['Embarked'] = df['Embarked'].map(embark_mapping)
-#	df = df.drop(['Embarked'], axis=1)
-#
-#	y = df['Survived'].values
-#
-#	df = df.drop(['Survived'], axis=1)
-#
-#	# impute missing values for the data
-#	imputer = imputer.fit(df)
-#	X = imputer.transform(df.values)
-#
-#	return X, y
-
-	y = df.pop('loss').values
 	imputer = imputer.fit(df)
 	X = imputer.transform(df.values)
-	return X, y
+	return X, y, ids
 
 
 # define some constants
-
+n_folds = 5
+verbose_tree = 1
+verbose_grid = 5
+n_jobs = -1
+random_state = 1
+criterion = 'mse'
 
 # read in the train dataset
-print('loading and processing training data...')
+print('loading training data...')
 df = pd.read_csv('train.csv', header=0)
 
-#print(df.head())
-#print(df.tail())
-
-X, y = build_features(df)
-print(X)
-print(y)
-import sys
-sys.exit('quitting to explore the dataset\n')
+print('processing training data...')
+X, y, ids = build_features(df)
+#print(X)
+#print(y)
+#print(ids)
 
 # create a Random Forest Classifier
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.cross_validation import StratifiedKFold
-from sklearn.grid_search import RandomizedSearchCV
 from sklearn.grid_search import GridSearchCV
-from sklearn.cross_validation import StratifiedKFold
-from sklearn.metrics import accuracy_score
 
 print('tuning hyperparameters with grid search...')
-n_estimators_range = [10, 20, 30, 40, 50, 60]
+n_estimators_range = [10, 20, 30]
 max_features_range = ['sqrt']
-max_depth_range = [4, 8, 12, 16]
+max_depth_range = [4, 8]
 
 # create a tree to train the models on
-tree = RandomForestClassifier(criterion='entropy',
-	random_state=1, n_jobs=-1)
+tree = RandomForestRegressor(criterion=criterion, verbose=verbose_tree, n_jobs=n_jobs,
+	random_state=random_state)
 param_grid = {'n_estimators': n_estimators_range, 'max_features': max_features_range,
 	'max_depth': max_depth_range}
 
-# some initialization parameters for my implementation of a grid search
-acc_max = 0
-n_folds = 4
-tree_max = RandomForestClassifier(criterion='entropy')
-n_estimators_max = 0
-max_features_max = 0
-max_depth_max = 0
-# splits the dataset in a stratified way
-skf = StratifiedKFold(y, n_folds=n_folds, random_state=True, shuffle=True)
-for n_estimators in n_estimators_range:
-	for max_features in max_features_range:
-		for max_depth in max_depth_range:
-			#print(n_estimators, max_features, max_depth)
-			acc_avg = 0
-			acc_low = 1.0
-			acc_high = 0.0
-			for train_index, test_index in skf:
-				tree = RandomForestClassifier(criterion='entropy',
-        				random_state=1, n_jobs=-1, n_estimators=n_estimators,
-					max_features=max_features, max_depth=max_depth)
-				tree.fit(X[train_index], y[train_index])
-				output = tree.predict(X[test_index])
-				acc = accuracy_score(y[test_index], output)
-				acc_avg = acc_avg + acc
-				if acc > acc_high:
-					acc_high = acc
-				if acc < acc_low:
-					acc_low = acc
-			#print(acc_low, acc_high)
-			acc_avg = acc_avg / float(n_folds)
-			#print(acc_avg)
-			if (acc_avg > acc_max):
-				tree_max = tree
-				acc_max = acc_avg
-				n_estimators_max = n_estimators
-				max_features_max = max_features
-				max_depth_max = max_depth
-
-print(acc_max, n_estimators_max, max_features_max, max_depth_max)
 gs = GridSearchCV(estimator=tree,
 	param_grid=param_grid,
-	scoring='accuracy',
-	cv=10, n_jobs=-1, verbose=1)
+	cv=n_folds, n_jobs=n_jobs, verbose=verbose_grid)
 gs = gs.fit(X, y)
 print('best score from grid search: %.3f' % gs.best_score_)
 print(gs.best_params_)
@@ -169,42 +89,45 @@ max_depth_gs = best['max_depth']
 max_features_gs = best['max_features']
 
 # read in the test data and predict the outputs
-print('reading and processing the test set data...')
+print('reading the whole data set...')
 df_all = pd.read_csv('train.csv', header=0)
+train_rows, train_cols = df_all.shape
+print(train_rows, train_cols)
 df_test = pd.read_csv('test.csv', header=0)
+test_rows, test_cols = df_test.shape
+print(test_rows, test_cols)
 df_all = df_all.append(df_test)
+print(df_all.shape)
 
-X, y = build_features(df_all)
-train_X = X[:891, :]
-test_X = X[891:, :]
+print('processing the whole data set...')
+X, y, ids = build_features(df_all)
+train_X = X[:train_rows, :]
+test_X = X[train_rows:, :]
 
-train_y = y[:891]
-#test_y = y[891:]
+train_y = y[:train_rows]
+#test_y = y[train_rows:]
+
+ids_train = ids[:train_rows]
+ids_test = ids[train_rows:]
 
 # train on all of the train data
 print('training on all training data...')
-tree_test = RandomForestClassifier(criterion='entropy',
-	n_estimators=n_estimators_max, max_features=max_features_max,
-	max_depth=max_depth_max, random_state=1, n_jobs=-1)
-tree_test_gs = RandomForestClassifier(criterion='entropy',
+tree_test_gs = RandomForestRegressor(criterion=criterion,
 	n_estimators=n_estimators_gs, max_features=max_features_gs,
-	max_depth=max_depth_gs, random_state=1, n_jobs=-1)
-tree_test.fit(train_X, train_y)
+	max_depth=max_depth_gs, random_state=random_state, n_jobs=n_jobs)
 tree_test_gs.fit(train_X, train_y)
 
 # predict the test set output
 print('predicting the test output...')
-test_pred = tree_test.predict(test_X).astype(int)
-#test_pred = tree_test_gs.predict(test_X).astype(int)
+test_pred = tree_test_gs.predict(test_X).astype(float)
 
 # write output to a file
 import csv
-outputFile = 'titanicSurvivors.csv'
+outputFile = 'allstateClaims.csv'
 print('writing output to %s...' % outputFile)
 prediction_file = open(outputFile, 'w')
 open_file_object = csv.writer(prediction_file)
-open_file_object.writerow(['PassengerId', 'Survived'])
-ids = list(range(892, 1310))
-open_file_object.writerows(zip(ids, test_pred))
+open_file_object.writerow(['id', 'loss'])
+open_file_object.writerows(zip(ids_test, test_pred))
 prediction_file.close()
 print('completed')
